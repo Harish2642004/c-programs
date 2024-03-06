a3c2327b69fecacb1f8d71263351d35e178b6654
Harish2642004 <120390677+Harish2642004@users.noreply.github.com>
2024-03-06 23:42:05 +0530

    Add files via upload

---
+++ rand uppercase.c
@@ -0,0 +1,11 @@
+/*Generate a random uppercase letter:*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+ {
+    char randomLetter = 'A' + rand() % 26;
+
+    printf("Random uppercase letter: %c\n", randomLetter);
+    return 0;
+}
